2a889704ba96244d8f41c3ae500788578d1a3f05
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-11 16:30:57 +0200

    Update cercle2.php

--- cercle2.php
+++ cercle2.php
@@ -1,6 +1,6 @@
 <?php // VA VERS REQUETS_POINTS
 // décide placement néons
-include_once("fonctions_positions.php");
+require_once("fonctions_positions.php");
 //var_dump($total_points);
 switch ($i){
 
@@ -45,3 +45,85 @@ switch ($i){
         break;
 
 }// fin case
+
+
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(162.195 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(-161.983 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(54.0612 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(-125.835 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="19.0025" height="20" transform="rotate(-90 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(125.964 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(-53.7143 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(-17.5918 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.6738" height="20" transform="rotate(18.1438 310.153 313.847)" fill="#00EBEC"></rect>
+// <rect x="301" y="313.847" width="18.672" height="20" transform="rotate(90 310.153 313.847)" fill="#00EBEC"></rect>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // switch ($i){
+    //     case 1 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(162.195 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 2 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(-161.983 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 3 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(54.0612 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 4 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(-125.835 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 5 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="19.0025" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(-90 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 6 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(125.964 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 7 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(-53.7143 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 8 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(-17.5918 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 9 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.6738" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(18.1438 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    //     case 10 : 
+    //         array_push($tableau_svg, '<rect x="301" y="' . placement_y($i, $data, $date, $total_points) . '" width="18.672" height="' . placement_height($i, $data, $date, $total_points) . '" transform="rotate(90 310.153 313.847)" fill="#00EBEC"></rect>';
+    //         break;
+
+    // }// fin case
+
+
+
+
+
+
+
+
