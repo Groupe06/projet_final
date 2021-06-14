d6aff4b2801badff6184409e21892b0338358dbc
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-14 16:44:02 +0200

    test bar

--- barChart.php
+++ barChart.php
@@ -4,6 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title></title>
+  <?php require_once("connexion_bdd.php"); ?>
 </head>
 
 <style>
@@ -28,7 +29,7 @@
         var ctx = document.getElementById("myChart").getContext("2d");
         
         // Valeur des labels 
-        var labelValues = ["Iron Man", "Spider-Man : Far From home"]
+        //var labelValues = ["Iron Man", "Spider-Man : Far From home"]
     
         // Création du graphique 
         var myChart = new Chart(ctx, {
@@ -39,7 +40,7 @@
                     barPercentage: 1, // Réglage de la largeur des bars - En % - Basé sur la taille du canva
                     categoryPercentage: 0.6, // Réglage de l'espace entre les bars
                     label: "Salaire ",
-                    data: [500000, 75000000], // TODO PHP
+                    data: [<?= $data["salaire_premier_film"],  $data["salaire_dernier_film"] ?>], // TODO PHP
                     backgroundColor: ["#41BBC1", '#41BBC1'], //Couleur des barres
                     borderColor: ["#41BBC1", "#41BBC1"], 
                     borderWidth: 1.2 // Epaisseur de la bordure 
