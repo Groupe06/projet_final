0136ff83d590b53f66a96e51b8dc36b7b4e8171d
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-15 16:16:57 +0200

    Update fonction_affichage.php

--- fonction_affichage.php
+++ fonction_affichage.php
@@ -1,17 +1,23 @@
 <?php 
-function affichage_personnage($data){
-    try{
-        $bdd = new PDO("mysql:host=localhost:3306;dbname=marvel", "root", "");
-    }catch(Exception $e){
-        die('Erreur : ' . $e->getMessage());
-    }
+function affichage_personnage($bdd){
+    // try{
 
-    // $requete_affichage = ("SELECT * FROM super_heros WHERE nom = :nom");
-    // $requete_affichage = $bdd->execute([
-    //     "nom" => "Ant-Man"
-    // ]);
-    $affichage = "okkkkkkk";   //$requete_affichage->fetchAll(PDO::FETCH_ASSOC);
-    return "<script> alert('". $affichage ."')  </script>";
+    //     $bdd = new PDO("mysql:host=localhost:3306;dbname=marvel", "root", "");
+    
+    // }catch(Exception $e){
+    //     die('Erreur : ' . $e->getMessage());
+    // }
+
+
+    $nom = "<script>recuperation_nom()</script>";
+    $requete_affichage = $bdd->prepare("SELECT * FROM super_heros WHERE super_heros.nom = :nom");
+    $requete_affichage->execute([
+        "nom" => "Ant Man"
+    ]);
+
+    $reponse_affichage = $requete_affichage->fetch(PDO::FETCH_ASSOC);
+    var_dump($reponse_affichage);
+    var_dump($nom);
 
     
 }
