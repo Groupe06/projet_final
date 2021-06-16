88f7b6691d126ae60d5606f1b7bd04de7e9af76d
ValentinR01 <vale.vr@icloud.com>
2021-06-16 18:05:08 +0200

    Chart update

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
-// donuts
-    // nb films solo
-    // nb films autres (total - solo)
-
-//barre
-    // premier salaire
-    //dernier salaire
-    var nom = "Iron Man";
+    
+    var nom = "Iron-Man";
+    var id = 0;
 
     //fetch("test.json")
     fetch("personnages.json")
@@ -15,9 +10,56 @@
         }
         })
         .then(function(value) {
-        //alert(value.comments[0].message);
-        alert(value[0][nom].salaire_dernier_film);
         console.log(value)
+        //alert(value.comments[0].message);
+
+        //Déclaration des variables nécessaires
+        const augmentation = value[id][nom].augmentation_salaire_film;
+        const  debutMcu = value[id][nom].annee_creation;
+        const mort = value[id][nom].mort;
+        const premierSalaire = value[id][nom].salaire_premier_film;
+        const dernierSalaire = value[id][nom].salaire_dernier_film;
+        const filmSolo = value[id][nom].nombre_films_solo;
+        const filmMcu = value[id][nom].nombre_films_totaux - filmSolo;
+        const envieContinuer = value[id][nom].envie_continuer;
+        var currentYear = new Date().getFullYear();
+
+        // Augmentation de salaire
+        document.getElementById("augmentationSalaire").textContent = augmentation + "$";
+
+
+        // Calcul du nombre d'année
+        if (mort === null) {
+            var Nb_annee = currentYear - debutMcu
+            document.getElementById("Nb_annee").textContent = Nb_annee
+        }
+        else {
+            var Nb_annee =  mort - debutMcu
+            document.getElementById("Nb_annee").textContent = Nb_annee
+        }
+
+
+        // Envie de continuer de l'acteur 
+        let divEnvie = document.getElementById("envieActeur");
+        if (envieContinuer === 'Y') {
+            divEnvie.textContent = 'Oui';
+        }
+        else if (envieContinuer === 'N') {
+            divEnvie.textContent = 'Oui';
+        }
+        else {
+            divEnvie.textContent = 'N/A';
+        }
+
+        // Création du graphique en Bar
+        let dataBar = [premierSalaire, dernierSalaire];
+        Graphique_Bar(dataBar);
+
+
+        //Création du graphique 
+        let dataDoughnut = [filmSolo, filmMcu];
+        Graphique_Doughnut(dataDoughnut);
+        
         })
         .catch(function(err) {
         // Une erreur est survenue
