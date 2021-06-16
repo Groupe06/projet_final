de047375bfd59b00a81b92c842f48a1079eddcad
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-16 11:42:20 +0200

    Update requetes_points.php

--- requetes_points.php
+++ requetes_points.php
@@ -39,6 +39,10 @@ if($reponse):
             //calcul des points
             $points_anciennetee = 12.5 - $anciennetee * 1.25;
 
+            //empeche inférieur à 0
+            if($points_anciennetee < 0):
+                $points_anciennetee = 0;
+            endif;
             
         
         //si mort : 
@@ -53,15 +57,30 @@ if($reponse):
         // section points films solos  
         $points_films_solos = 3.75 - $data["nombre_films_solo"] * 0.375;
 
-
+        // si point < 0 
+        if($points_films_solos < 0):
+            $points_films_solos = 0;
+        endif;
 
         
         // section films totaux
+
         $points_totaux_films =12.5 - $data["nombre_films_totaux"] * 1.25;
+
+        //si point < 0 
+        if( $points_totaux_films < 0 ):
+            $points_totaux_films = 0;
+        endif;
+
 
 
         // section dernier film     
         $points_dernier_salaire = $data["salaire_dernier_film"] / 4375000;
+
+        //si point < 0
+        if ( $points_dernier_salaire < 0 ):
+            $points_dernier_salaire = 0; 
+        endif;
 
 
 
@@ -82,9 +101,6 @@ if($reponse):
         //calcul des points totaux 
         $total_points = $points_anciennetee + $points_films_solos + $points_totaux_films + $points_dernier_salaire + $points_continuitee;
 
-        if($total_points < 0){
-            $total_points = 0;
-        }
         //var_dump("while $i");
         require("cercle2.php");
         $i++;
