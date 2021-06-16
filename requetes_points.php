3b8f6e788164d009a2f1e16e4592fe63f652ca65
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-16 11:48:05 +0200

    Update requetes_points.php

--- requetes_points.php
+++ requetes_points.php
@@ -29,7 +29,9 @@ if($reponse):
         // va pousser données de chaque personnage dans dictionnaire $donnees_json pour envoi
         array_push($donnees_json, $donnees_personnage );
 
-        // début comptage points anciennetée :
+
+
+// SECTION points anciennetée :
 
         //si vivant :
         if(!$data["mort"]):
@@ -51,19 +53,23 @@ if($reponse):
             $points_anciennetee = 0 ;
             
         endif;
+// FIN section 
 
 
 
-        // section points films solos  
+// SECTION points films solos  
+
         $points_films_solos = 3.75 - $data["nombre_films_solo"] * 0.375;
 
         // si point < 0 
         if($points_films_solos < 0):
             $points_films_solos = 0;
         endif;
-
+//FIN section films solos
         
-        // section films totaux
+
+
+// SECTION films totaux
 
         $points_totaux_films =12.5 - $data["nombre_films_totaux"] * 1.25;
 
@@ -71,20 +77,23 @@ if($reponse):
         if( $points_totaux_films < 0 ):
             $points_totaux_films = 0;
         endif;
+// FIN totaux films
 
 
 
-        // section dernier film     
+// SECTION dernier film     
+
         $points_dernier_salaire = $data["salaire_dernier_film"] / 4375000;
 
         //si point < 0
         if ( $points_dernier_salaire < 0 ):
             $points_dernier_salaire = 0; 
         endif;
+// FIN dernier salaire
 
 
+//SECTION continuitee  
 
-        //section continuitee  
         switch ($data["envie_continuer"]){
             case "Y" : 
                 $points_continuitee = 10;
@@ -94,11 +103,11 @@ if($reponse):
                 break;
             default : 
                 $points_continuitee = 5 ;
-        }
-        
+        } 
+// FIN continuitee
 
 
-        //calcul des points totaux 
+//calcul des points totaux 
         $total_points = $points_anciennetee + $points_films_solos + $points_totaux_films + $points_dernier_salaire + $points_continuitee;
 
         //var_dump("while $i");
