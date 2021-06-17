0252ab3fdda298ddd6bb8747722223a819a2bad6
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-17 09:39:58 +0200

    Update requetes_points.php

--- requetes_points.php
+++ requetes_points.php
@@ -13,14 +13,8 @@ if($reponse):
 // WHILE -> tant que tous les personnages ne sont pas passés 
     while ($data = $reponse->fetch()):
 
-        // Récupération données pour remplissage "$donnes_json" puis mise envoi dans fichier json 
-        // chaque tour de boucle sera nouveau perso 
-        $donnees_personnage = array( $data["nom"] => array( "annee_creation" => $data["annee_creation"], 
-        "nombre_films_solo" => $data["nombre_films_solo"],
-        "nombre_films_totaux" => $data["nombre_films_totaux"], "salaire_dernier_film" => $data["salaire_dernier_film"], "envie_continuer" => $data["envie_continuer"], 
-        "mort" => $data["mort"], 
-        "salaire_premier_film" => $data["salaire_premier_film"], "augmentation_salaire_film" => $data["augmentation_salaire_film"] ));
-
+        
+        
 
 
 
@@ -120,6 +114,14 @@ if($reponse):
 
     endwhile; 
 //FIN while 
+
+    // Récupération données pour remplissage "$donnes_json" puis mise envoi dans fichier json 
+    // chaque tour de boucle sera nouveau perso 
+    $donnees_personnage = array( $data["nom"] => array( "annee_creation" => $data["annee_creation"], 
+    "nombre_films_solo" => $data["nombre_films_solo"],
+    "nombre_films_totaux" => $data["nombre_films_totaux"], "salaire_dernier_film" => $data["salaire_dernier_film"], "envie_continuer" => $data["envie_continuer"], 
+    "mort" => $data["mort"], 
+    "salaire_premier_film" => $data["salaire_premier_film"], "augmentation_salaire_film" => $data["augmentation_salaire_film"], "points" => $total_points ));
 
 
     //encodage php en json
