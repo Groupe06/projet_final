fad91642373c5e6c3a0993dcaa5fb5d508d3807d
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-17 11:58:55 +0200

    Update connexion_bdd.php

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,9 +1,20 @@
 <?php
 // connexion à la bdd
+// try{
+
+//     $bdd = new PDO("mysql:host=localhost:3306;dbname=marvel", "root", "");
+
+// }catch(Exception $e){
+//     die('Erreur : ' . $e->getMessage());
+// }
+
+// // requete SQL
+// $requete = ("SELECT * FROM super_heros");
+// $reponse = $bdd->query($requete);
+
+// connexion à la bdd
 try{
-
-    $bdd = new PDO("mysql:host=localhost:3306;dbname=marvel", "root", "");
-
+    $bdd = new PDO("mysql:host=db5002899289.hosting-data.io:3306;dbname=dbs2325018", "dbu1981188", "Groupe062021+");
 }catch(Exception $e){
     die('Erreur : ' . $e->getMessage());
 }
@@ -12,3 +23,4 @@ try{
 $requete = ("SELECT * FROM super_heros");
 $reponse = $bdd->query($requete);
 ?>
+
