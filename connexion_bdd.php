8f64a9ac38f0f10ea8a906c2094e7a045cb9a19d
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-06-17 12:19:27 +0200

    Update connexion_bdd.php

--- connexion_bdd.php
+++ connexion_bdd.php
@@ -1,20 +1,9 @@
 <?php
-// connexion à la bdd
-// try{
 
-//     $bdd = new PDO("mysql:host=localhost:3306;dbname=marvel", "root", "");
-
-// }catch(Exception $e){
-//     die('Erreur : ' . $e->getMessage());
-// }
-
-// // requete SQL
-// $requete = ("SELECT * FROM super_heros");
-// $reponse = $bdd->query($requete);
-
-// connexion à la bdd
 try{
-    $bdd = new PDO("mysql:host=db5002899289.hosting-data.io:3306;dbname=dbs2325018", "dbu1981188", "Groupe062021+");
+
+    $bdd = new PDO("mysql:host=localhost:3306;dbname=marvel", "root", "");
+
 }catch(Exception $e){
     die('Erreur : ' . $e->getMessage());
 }
@@ -22,5 +11,16 @@ try{
 // requete SQL
 $requete = ("SELECT * FROM super_heros");
 $reponse = $bdd->query($requete);
+
+
+// try{
+//     $bdd = new PDO("mysql:host=db5002899289.hosting-data.io:3306;dbname=dbs2325018", "dbu1981188", "Groupe062021+");
+// }catch(Exception $e){
+//     die('Erreur : ' . $e->getMessage());
+// }
+
+// // requete SQL
+// $requete = ("SELECT * FROM super_heros");
+// $reponse = $bdd->query($requete);
 ?>
 
